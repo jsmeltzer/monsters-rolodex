ee3065f53436d8be58c10ecfe4abef842cad9817
jsmeltzer <jeremy@webguyaz.com>
2020-03-25 17:51:01 -0700

    Updates

---
+++ precache-manifest.b458dfa7914cf044ef18d5e584a50f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1348471b8ed1e2848b740514cdae9877",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2f71c95414620a5c07d8",
+    "url": "/monsters-rolodex/static/css/main.bbca2bc4.chunk.css"
+  },
+  {
+    "revision": "8aef2538accc0c8ef9b8",
+    "url": "/monsters-rolodex/static/js/2.cb54538f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.cb54538f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f71c95414620a5c07d8",
+    "url": "/monsters-rolodex/static/js/main.abcd9b86.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
